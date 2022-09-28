736c73e0f4e5d54bf26f8add5feaff27acb1f49b
Sheika Abuthair <105582784+Abuthair97@users.noreply.github.com>
2022-09-28 23:29:14 +0530

    Update Camera.java

--- spring-cameraresponse-restapi/src/main/java/com/cameraapp/model/Camera.java
+++ spring-cameraresponse-restapi/src/main/java/com/cameraapp/model/Camera.java
@@ -14,11 +14,15 @@ public class Camera {
     @Id
     @GeneratedValue(generator = "camera_gen",strategy = GenerationType.AUTO)
     @SequenceGenerator(name = "camera_gen",sequenceName = "camera_sequence",initialValue = 1,allocationSize = 1)
+    
     private Integer cameraId;
+    
     @Column(length = 30)
     private String model;
+    
     @Column(length = 30)
     private String cameraType;
+    
     @Column(length = 30)
     private  String lensType;
     private  double price;
